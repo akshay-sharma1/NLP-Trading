f100aeab18d0112aec5be2edd153d4352937b6d6
Akshay Sharma <aksharma@macbook-pro.lan>
2020-08-26 23:54:10 -0500

    change worker configuration

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run(environ.get('PORT'))
+app.run(host='0.0.0.0', port=environ.get('PORT'))
 
 
 def get_orig_tweet_link(tweet_info):
